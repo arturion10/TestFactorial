e4a184aa9746d8925cad2bef61f448d3c8f6ebd8
Shepelkov Artur <shepelkovartur@gmail.com>
2022-03-23 21:54:45 +0300

    Добавьте файлы проекта.

---
+++ TestFactorial/Program.cs
@@ -0,0 +1,23 @@
+﻿bool isValidNumber = int.TryParse(Console.ReadLine(), out int number);
+if (isValidNumber)
+{
+    Console.WriteLine($"!" + number + " = " + Factorial(number));
+}
+else
+{
+    Console.WriteLine("Введите целое число");
+}
+
+int Factorial(int number)
+{
+    var fact = 1;
+    if (number == 0)
+    {
+        return 1;
+    }
+    for (int i = number; i > 1; i--)
+    {
+        fact *= i;
+    }
+    return fact;
+}
